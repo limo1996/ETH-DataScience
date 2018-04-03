0cf8412afe0378d3c8ba07e865086e03c6293de7
Florian Koch <flkoch@dev.fkoch.ch>
2018-04-03 18:03:45 +0200

    Added zones with driving prohibited.

---
+++ prog/preprocess/fahrverbot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Florian Koch
+@license: All rights reserved
+"""
+import pandas as pd
+import json
+
+with open('../../data/city_of_zurich/fahrverbotszone.json') as data_file:
+  data = json.load(data_file)
+  df = pd.io.json.json_normalize(data, ['features', ['geometry', 'coordinates']],['name', ['features', 'properties', 'zonenname'], ['features', 'properties', 'bestehend']])
+
+df.columns = ['coordinate array', 'type', 'name', 'present']
+# print(df)
+df.to_csv('../../data/prepared/drivin_prohibited.csv')
